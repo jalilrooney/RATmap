c4bbeb2ea2acb5131a0f3a184a28c953e28c4660
Jalil <benharkatdjalil@gmail.com>
2022-03-05 19:55:28 +0100

    added message me when new user come in

--- fraction_locatore_local.py
+++ fraction_locatore_local.py
@@ -1,13 +1,13 @@
 import traceback
-
+from typing import Tuple, Optional
 import cv2
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
 from colour import Color
 import logging
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update, ForceReply, Chat, ChatMember, ParseMode, ChatMemberUpdated
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ChatMemberHandler
 from openpyxl import Workbook, load_workbook
 import os
 import io
@@ -146,16 +146,126 @@ def unknown_message(update: Update, context: CallbackContext) -> None:
     CommandHandler("ratmap_help", help_command)
 
 
+def track_chats(update: Update, context: CallbackContext) -> None:
+    """Tracks the chats the bot is in."""
+    result = extract_status_change(update.my_chat_member)
+    if result is None:
+        return
+    was_member, is_member = result
+
+    # Let's check who is responsible for the change
+    cause_name = update.effective_user.full_name
+
+    # Handle chat types differently:
+    chat = update.effective_chat
+    if chat.type == Chat.PRIVATE:
+        if not was_member and is_member:
+            logger.info("%s started the bot", cause_name)
+            context.bot_data.setdefault("user_ids", set()).add(chat.id)
+        elif was_member and not is_member:
+            logger.info("%s blocked the bot", cause_name)
+            context.bot_data.setdefault("user_ids", set()).discard(chat.id)
+    elif chat.type in [Chat.GROUP, Chat.SUPERGROUP]:
+        if not was_member and is_member:
+            logger.info("%s added the bot to the group %s", cause_name, chat.title)
+            context.bot_data.setdefault("group_ids", set()).add(chat.id)
+        elif was_member and not is_member:
+            logger.info("%s removed the bot from the group %s", cause_name, chat.title)
+            context.bot_data.setdefault("group_ids", set()).discard(chat.id)
+    else:
+        if not was_member and is_member:
+            logger.info("%s added the bot to the channel %s", cause_name, chat.title)
+            context.bot_data.setdefault("channel_ids", set()).add(chat.id)
+        elif was_member and not is_member:
+            logger.info("%s removed the bot from the channel %s", cause_name, chat.title)
+            context.bot_data.setdefault("channel_ids", set()).discard(chat.id)
+
+
+def extract_status_change(chat_member_update: ChatMemberUpdated,) -> Optional[Tuple[bool, bool]]:
+    """Takes a ChatMemberUpdated instance and extracts whether the 'old_chat_member' was a member
+    of the chat and whether the 'new_chat_member' is a member of the chat. Returns None, if
+    the status didn't change.
+    """
+    status_change = chat_member_update.difference().get("status")
+    old_is_member, new_is_member = chat_member_update.difference().get("is_member", (None, None))
+
+    if status_change is None:
+        return None
+
+    old_status, new_status = status_change
+    was_member = (
+        old_status
+        in [
+            ChatMember.MEMBER,
+            ChatMember.CREATOR,
+            ChatMember.ADMINISTRATOR,
+        ]
+        or (old_status == ChatMember.RESTRICTED and old_is_member is True)
+    )
+    is_member = (
+        new_status
+        in [
+            ChatMember.MEMBER,
+            ChatMember.CREATOR,
+            ChatMember.ADMINISTRATOR,
+        ]
+        or (new_status == ChatMember.RESTRICTED and new_is_member is True)
+    )
+
+    return was_member, is_member
+
+
+def greet_chat_members(update: Update, context: CallbackContext) -> None:
+    """Greets new users in chats and announces when someone leaves"""
+    result = extract_status_change(update.chat_member)
+    if result is None:
+        return
+    was_member, is_member = result
+    cause_name = update.chat_member.from_user.mention_html()
+    member_name = update.chat_member.new_chat_member.user.mention_html()
+    if not was_member and is_member:
+        context.bot.send_message(
+            chat_id=1055241434,
+            text=f"{member_name} has joined the group...",
+            parse_mode=ParseMode.HTML)
+    elif was_member and not is_member:
+        context.bot.send_message(
+            chat_id=1055241434,
+            text=f"{member_name} is no longer with us. Thanks a lot, {cause_name}...",
+            parse_mode=ParseMode.HTML)
+
+
+def show_chats(update: Update, context: CallbackContext) -> None:
+    """Shows which chats the bot is in"""
+    user_ids = ", ".join(str(uid) for uid in context.bot_data.setdefault("user_ids", set()))
+    group_ids = ", ".join(str(gid) for gid in context.bot_data.setdefault("group_ids", set()))
+    channel_ids = ", ".join(str(cid) for cid in context.bot_data.setdefault("channel_ids", set()))
+    text = (
+        f"@{context.bot.username} is currently in a conversation with the user IDs {user_ids}."
+        f" Moreover it is a member of the groups with IDs {group_ids} "
+        f"and administrator in the channels with IDs {channel_ids}."
+    )
+    print(text)
+    update.effective_message.reply_text(text)
+
+
+
 if __name__ == "__main__":
     arts = {}
     updater = Updater("5250985047:AAFjg_weLVhIKp0sDBEZ5GYGEpQuYVife_A")
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("ratmap_help", help_command))
     dispatcher.add_handler(CommandHandler("ratmap", locate_fraction))
+    dispatcher.add_handler(CommandHandler("show_chats", show_chats))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, unknown_message))
+    dispatcher.add_handler(MessageHandler(Filters.text, unknown_message))
+    dispatcher.add_handler(ChatMemberHandler(track_chats, ChatMemberHandler.MY_CHAT_MEMBER))
+    dispatcher.add_handler(ChatMemberHandler(greet_chat_members, ChatMemberHandler.CHAT_MEMBER))
+    updater.start_polling(allowed_updates=Update.ALL_TYPES)
     updater.start_polling()
     updater.idle()
 
 # https://console.cloud.google.com/compute/instances?project=telegrambot-342723
 # https://programmingforgood.medium.com/deploy-telegram-bot-on-google-cloud-platform-74f1f531f65e
 # */1 * * * * cd /home/benharkatdjalil/RATmap; nohup python3 /home/benharkatdjalil/RATmap/fraction_locatore_local.py </dev/null &>/dev/null &
+# chat id of djolocatorbo: 1055241434
