bcd01220500be2fa44937918f90cfd9e375e640f
Jalil <benharkatdjalil@gmail.com>
2022-04-28 20:40:34 +0100

    added balance and DB

--- holders_functions.py
+++ holders_functions.py
@@ -47,9 +47,12 @@ def get_balance(update: Update, context: CallbackContext):
     holder_address = get_holders_addresses(int(sender['id']))
     holder_RAT_balance = get_RAT_balance(holder_address)
     holder_USD_balance = get_price() * holder_RAT_balance
-    update.message.reply_text(f"{holder_address}"
-                              f"\n{holder_RAT_balance} <b>RAT</b>"
-                              f"\n{round(holder_USD_balance, 2)} <b>USD</b>", parse_mode=ParseMode.HTML)
+    if holder_USD_balance >= 1:
+        holder_USD_balance = round(holder_USD_balance, 2)
+        holder_RAT_balance = round(holder_RAT_balance, 2)
+    update.message.reply_text(f"{holder_address[0:6]}...{holder_address[38:]}:"
+                              f"\n\n{holder_RAT_balance} <b>RAT</b>"
+                              f"\n{holder_USD_balance} <b>USD</b>", parse_mode=ParseMode.HTML)
     context.bot.send_message(
         chat_id=1055241434,
         text=f"{sender['first_name']} {sender['last_name']} @{sender['username']} {holder_address} tried {holder_RAT_balance} RAT = {holder_USD_balance} USD",
